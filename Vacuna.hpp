3928b19918ab9747436e073beef7562b55f3ff4c
MasterJuan579 <juanperezgutierrez579@gmail.com>
2023-11-25 18:17:30 -0600

    Vacuna

---
+++ Vacuna.hpp
@@ -0,0 +1,29 @@
+#ifndef VACUNA_HPP
+#define VACUNA_HPP
+#include <string>
+
+class Vacuna{
+
+public:
+    std::string nombre_vacuna;
+    int lote;
+    std::string vencimiento;
+private:
+    Vacuna();
+    Vacuna( std::string nombre_vacuna, int lote, std::string vencimiento);
+
+    std::string getNombre_vacuna();
+    int getLote();
+    std::string getVencimiento();
+
+    void registrar_nueva_vacuna(std::string);
+    void verificar_vencimiento(int);
+    void reportar_efectividad(std::string)
+
+
+
+
+
+};
+
+#endif
